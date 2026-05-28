0d871e57f0ca7e5f0ac203cec5312d7ccc388809
unknown <bonverry2004@gmail.com>
2026-05-28 20:10:03 +0700

    28-05-2026

--- src/pages/pengguna.jsx
+++ src/pages/pengguna.jsx
@@ -1,6 +1,25 @@
 import { Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Pengguna = () => {
+    const [user, setUser] = useState([]);
+
+    const selectPengguna = async () => {
+        const url = "http://localhost/inventarisweb/penggunaread.php"
+        try {
+            const response = await axios.get(url);
+            setUser(response.data["DATA"]);
+            // console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        selectPengguna();
+    }, []);
+
     return (
         <>
             <Text>Ini Halaman Pengguna</Text>
